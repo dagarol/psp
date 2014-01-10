d37070881552f903552a14b49c52cc97b00004ac
dagarol <david.ies.serpis@gmail.com>
2014-01-10 10:39:01 +0100

    subida prueba sencilla 1

---
+++ PExec/src/serpis/psp/Probar.java
@@ -0,0 +1,23 @@
+package serpis.psp;
+
+
+
+import java.io.*;
+
+
+public class Probar
+{
+    public static void main (String []args)
+    {
+	    try
+	    {
+	      
+	       Process p = Runtime.getRuntime().exec ("firefox");
+	    }
+	    catch (Exception e)
+	    {
+	       
+	    }
+    }
+}
+    
